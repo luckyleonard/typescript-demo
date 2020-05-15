644ff269948d7741dba71c60459ea17f6a01ef3e
luckyleonard <dzhu31@hotmail.com>
2020-05-15 15:33:43 +1000

    ✨ feat(login) achieve basic login logic
    - index.ts include cookie-session middleware
    - router.ts achieve login logic

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import router from './router';
 import bodyParser from 'body-parser';
-
+import cookieSession from 'cookie-session';
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use((req: Request, res: Response, next: NextFunction) => {
+  req.myName = 'leonard'; //添加的myname并不被Request支持
+  next();
+});
+
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['leonard-secret'],
+
+    // Cookie Options
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  })
+);
+
 app.use(router);
 
 app.listen(7001, () => {
--- src/router.ts
+++ src/router.ts
@@ -1,30 +1,92 @@
 import { Router, Request, Response } from 'express';
+import fs from 'fs';
+import path from 'path';
 import Crowller from './crowller';
 import NewsAnalyzer from './newsAnalyzer';
+
+interface RequestWithBody extends Request {
+  body: {
+    [key: string]: string | undefined;
+    //泛匹配
+  };
+}
 
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
-  res.send(`
+  const isLogin = req.session ? req.session.login : false;
+  if (isLogin) {
+    res.send(`    
     <html>
       <body>
-        <form method="post" action = "/getData">
+        <a href='/getData'>get data</a>
+        <a href='/showData'>show data</a>
+        <a href='/logout'>logout</a>
+      </body>
+    </html>`);
+  } else {
+    res.send(`
+    <html>
+      <body>
+        <form method="post" action = "/login">
+          username:
+          <input type="text" name="username"/>
+          password:
           <input type="password" name="password"/>
-          <button>submit</button>
+          <button>login</button>
         </form>
       </body>
     </html>
   `);
+  }
 });
 
-router.post('/getData', (req: Request, res: Response) => {
-  if (req.body.password === '123') {
+router.post('/login', (req: RequestWithBody, res: Response) => {
+  const { password, username } = req.body;
+  const isLogin = req.session ? req.session.login : false;
+  if (isLogin) {
+    res.send('you still login');
+  } else {
+    if (password === '123' && req.session) {
+      req.session.login = true;
+      res.send('login success');
+    } else {
+      res.send('login fail');
+    }
+  }
+});
+
+router.get('/logout', (req: Request, res: Response) => {
+  if (req.session) {
+    req.session.login = undefined;
+  }
+  res.redirect('/');
+});
+
+router.get('/getData', (req: RequestWithBody, res: Response) => {
+  const isLogin = req.session ? req.session.login : false;
+  if (isLogin) {
     const url = `https://news.ycombinator.com/`;
     const analyzer = NewsAnalyzer.getInstance();
     new Crowller(url, analyzer);
     res.send('get data Success!');
   } else {
-    res.send('your password is incorrect');
+    res.send(`please login <a href='/'>login</a>`);
+  }
+});
+
+router.get('/showData', (req: RequestWithBody, res: Response) => {
+  const isLogin = req.session ? req.session.login : false;
+  if (isLogin) {
+    try {
+      const filePath = path.resolve(__dirname, '../data/news.json');
+      const result = fs.readFileSync(filePath, 'utf-8');
+      res.json(JSON.parse(result));
+    } catch (e) {
+      res.send(`get data first <a href='/getData'>get data</a>`);
+    }
+  } else {
+    res.send(`please login <a href='/'>login</a>`);
   }
 });
 
